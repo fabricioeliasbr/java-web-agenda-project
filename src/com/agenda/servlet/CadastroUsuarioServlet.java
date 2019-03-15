89a2bca893036ec432f18cb65907e7205bb6abd6
Fabrício Elias <fabricio.silva226@etec.sp.gov.br>
2019-03-14 22:28:20 -0300

    Melhorei o formulário, criei a classe pessoa e iniciei a criação de um objeto

--- src/com/agenda/servlet/CadastroUsuarioServlet.java
+++ src/com/agenda/servlet/CadastroUsuarioServlet.java
@@ -6,10 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.agenda.model.Pessoa;
+
 public class CadastroUsuarioServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getParameter("nome");
-		System.out.println(request.getParameter("nome"));
+		request.getParameter("tel");
+/*		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("tel"));*/
 		
+		Pessoa pessoa;
+		pessoa = new Pessoa();
+		pessoa.setNome(request.getParameter("nome"));
+		pessoa.setEmail(request.getParameter("email"));
+		pessoa.setTelefone(request.getParameter("tel"));
+		pessoa.setEndereco(request.getParameter("endereco"));
 	}
 }
